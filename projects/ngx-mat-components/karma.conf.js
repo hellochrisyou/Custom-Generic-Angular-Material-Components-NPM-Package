9638e332d1f7e502163d2ca847a3babfef9735df
Chris You <28457425+hellochrisyou@users.noreply.github.com>
2020-06-05 05:55:27 -0400

    test

--- projects/ngx-mat-components/karma.conf.js
+++ projects/ngx-mat-components/karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    browsers: ['Chrome'],
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
