75382279849114e0f0c180477169ee76bc707038
Chris You <28457425+hellochrisyou@users.noreply.github.com>
2020-06-05 05:45:13 -0400

    space

--- projects/ngx-mat-components/src/lib/ngx-mat-components.component.spec.ts
+++ projects/ngx-mat-components/src/lib/ngx-mat-components.component.spec.ts
@@ -1,25 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgxMatComponentsComponent } from './ngx-mat-components.component';
-
 describe('NgxMatComponentsComponent', () => {
   let component: NgxMatComponentsComponent;
   let fixture: ComponentFixture<NgxMatComponentsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NgxMatComponentsComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [NgxMatComponentsComponent]
+    });
     fixture = TestBed.createComponent(NgxMatComponentsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
-
-  it('should create', () => {
+  it('can load instance', () => {
     expect(component).toBeTruthy();
   });
 });
+ 
